aa5f8891e494d680bf50222c10f833945f23b95b
Mike Turley <mike@miketurley.com>
2013-03-16 19:13:50 -0400

    fixing RelativeLayout casts in fragment files

--- luper-android/src/com/teamluper/luper/TabFriendsFragment.java
+++ luper-android/src/com/teamluper/luper/TabFriendsFragment.java
@@ -7,12 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class TabFriendsFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater infl, ViewGroup vg, Bundle state) {
     if(vg == null) return null;
-    return (LinearLayout)infl.inflate(R.layout.tab_friends_layout, vg, false);
+    return (RelativeLayout)infl.inflate(R.layout.tab_friends_layout, vg, false);
   }
 }
