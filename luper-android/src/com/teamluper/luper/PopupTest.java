24623bb65652a9b715a0cefe7a3ba3778ecf2381
xianc <xianc92@gmail.com>
2013-03-27 23:54:28 -0400

    Popup Menu Test

---
+++ luper-android/src/com/teamluper/luper/PopupTest.java
@@ -0,0 +1,47 @@
+package com.teamluper.luper;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class PopupTest extends SherlockActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Button button = new Button(this);
+        button.setText("Project_Name");
+        button.setOnClickListener(listener);
+        registerForContextMenu(button);
+
+        setContentView(button);
+    }
+
+    View.OnClickListener listener = new View.OnClickListener() {
+        public void onClick(View v) {
+            openContextMenu(v);
+        }
+    };
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.contextmenu, menu);
+        menu.setHeaderTitle("Watcha want to do?");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+        case R.id.menu_delete:
+            return true;
+        case R.id.menu_edit:
+            return true;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+}
