db602990e65828d13603cea4d64dcb7eab924f77
Mike Turley <mike@miketurley.com>
2013-03-29 20:54:19 -0400

    adding SQLite database stuff (unused so far)

---
+++ luper-android/res/raw/create_sqlite_tables.sql
@@ -0,0 +1,4 @@
+create table foo (
+  _id int primary key autoincrement,
+  comment text not null
+);
---
+++ luper-android/src/com/teamluper/luper/LuperSQLiteHelper.java
@@ -0,0 +1,40 @@
+package com.teamluper.luper;
+
+import java.io.InputStream;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+public class LuperSQLiteHelper extends SQLiteOpenHelper {
+  private static final String DATABASE_NAME = "luperlocal.db";
+  private static final int DATABASE_VERSION = 1;
+  
+  private Context context;
+  
+  public LuperSQLiteHelper(Context c) {
+    super(c, DATABASE_NAME, null, DATABASE_VERSION);
+    context = c;
+  }
+  
+  @Override
+  public void onCreate(SQLiteDatabase database) {
+    InputStream in = context.getResources().openRawResource(R.raw.create_sqlite_tables);
+    java.util.Scanner s = new java.util.Scanner(in).useDelimiter("\\A");
+    String sql = s.hasNext() ? s.next() : "";
+    database.execSQL(sql);
+  }
+  
+  @Override
+  public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    Log.w(LuperSQLiteHelper.class.getName(),
+        "Upgrading database from version " + oldVersion + " to "
+            + newVersion + ", which will destroy all old data");
+    InputStream in = context.getResources().openRawResource(R.raw.drop_sqlite_tables);
+    java.util.Scanner s = new java.util.Scanner(in).useDelimiter("\\A");
+    String sql = s.hasNext() ? s.next() : "";
+    db.execSQL(sql);
+    onCreate(db);
+  }
+}
