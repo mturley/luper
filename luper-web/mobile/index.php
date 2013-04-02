ef214b001ccefcc1b683ac7600ad3e42b3d9732b
Mike Turley <mike@miketurley.com>
2013-04-02 16:59:40 -0400

    adding mobile site

---
+++ luper-web/mobile/index.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>Luper mobile site</title>
+  </head>
+  <body>
+    <h1>Luper Beta Release (4/2/2013)</h1>
+    <h2><a href="/download/LuperBeta.apk">Click Here to Download LuperBeta.apk</a></h2>
+    <h3>For more details, visit this page on a desktop browser.</h3>
+    <h4>(proper mobile site style coming soon)</h4>
+  </body>
+</html>
