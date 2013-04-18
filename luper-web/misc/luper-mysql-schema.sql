bfdfa185db4f9853b3e7edabf43fb2b34d6a5e31
Mike Turley <mike@miketurley.com>
2013-04-17 22:04:11 -0400

    updating the MySQL schema

---
+++ luper-web/misc/luper-mysql-schema.sql
@@ -0,0 +1,95 @@
+-- Adminer 3.6.3 MySQL dump
+
+SET NAMES utf8;
+SET foreign_key_checks = 0;
+SET time_zone = 'SYSTEM';
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP DATABASE IF EXISTS `luper`;
+CREATE DATABASE `luper` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `luper`;
+
+DROP TABLE IF EXISTS `Clips`;
+CREATE TABLE `Clips` (
+  `_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ownerUserID` int(11) NOT NULL,
+  `parentTrackID` int(11) NOT NULL,
+  `audioFileID` int(11) NOT NULL,
+  `startTime` int(11) NOT NULL,
+  `durationMS` int(11) DEFAULT NULL,
+  `loopCount` int(11) NOT NULL,
+  `isLocked` tinyint(1) NOT NULL DEFAULT '0',
+  `playbackOptions` text,
+  PRIMARY KEY (`_id`,`ownerUserID`),
+  KEY `ownerUserID` (`ownerUserID`),
+  KEY `parentTrackID` (`parentTrackID`),
+  KEY `audioFileID` (`audioFileID`),
+  CONSTRAINT `Clips_ibfk_3` FOREIGN KEY (`audioFileID`) REFERENCES `Files` (`_id`),
+  CONSTRAINT `Clips_ibfk_1` FOREIGN KEY (`ownerUserID`) REFERENCES `Users` (`_id`),
+  CONSTRAINT `Clips_ibfk_2` FOREIGN KEY (`parentTrackID`) REFERENCES `Tracks` (`_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `Files`;
+CREATE TABLE `Files` (
+  `_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ownerUserID` int(11) NOT NULL,
+  `clientFilePath` varchar(256) NOT NULL,
+  `serverFilePath` varchar(256) DEFAULT NULL,
+  `fileFormat` varchar(8) NOT NULL,
+  `bitrate` double DEFAULT NULL,
+  `durationMS` int(11) DEFAULT NULL,
+  `isReadyOnClient` tinyint(1) NOT NULL DEFAULT '0',
+  `isReadyOnServer` tinyint(1) NOT NULL DEFAULT '0',
+  `renderSequenceID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`_id`,`ownerUserID`),
+  KEY `ownerUserID` (`ownerUserID`),
+  KEY `renderSequenceID` (`renderSequenceID`),
+  CONSTRAINT `Files_ibfk_2` FOREIGN KEY (`renderSequenceID`) REFERENCES `Sequences` (`_id`),
+  CONSTRAINT `Files_ibfk_1` FOREIGN KEY (`ownerUserID`) REFERENCES `Users` (`_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `Sequences`;
+CREATE TABLE `Sequences` (
+  `_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ownerUserID` int(11) NOT NULL,
+  `title` int(11) NOT NULL,
+  `sharingLevel` int(11) NOT NULL DEFAULT '0',
+  `playbackOptions` int(11) DEFAULT NULL,
+  PRIMARY KEY (`_id`,`ownerUserID`),
+  KEY `ownerUserID` (`ownerUserID`),
+  CONSTRAINT `Sequences_ibfk_1` FOREIGN KEY (`ownerUserID`) REFERENCES `Users` (`_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `Tracks`;
+CREATE TABLE `Tracks` (
+  `_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ownerUserID` int(11) NOT NULL,
+  `parentSequenceID` int(11) NOT NULL,
+  `isMuted` int(11) NOT NULL DEFAULT '0',
+  `isLocked` int(11) NOT NULL DEFAULT '0',
+  `playbackOptions` text,
+  PRIMARY KEY (`_id`,`ownerUserID`),
+  KEY `ownerUserID` (`ownerUserID`),
+  KEY `parentSequenceID` (`parentSequenceID`),
+  CONSTRAINT `Tracks_ibfk_2` FOREIGN KEY (`parentSequenceID`) REFERENCES `Sequences` (`_id`),
+  CONSTRAINT `Tracks_ibfk_1` FOREIGN KEY (`ownerUserID`) REFERENCES `Users` (`_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `Users`;
+CREATE TABLE `Users` (
+  `_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `passwordHash` varchar(512) NOT NULL,
+  `isActiveUser` tinyint(1) NOT NULL DEFAULT '0',
+  `linkedFacebookID` int(11) DEFAULT NULL,
+  `preferences` text,
+  PRIMARY KEY (`_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- 2013-04-18 01:55:29
