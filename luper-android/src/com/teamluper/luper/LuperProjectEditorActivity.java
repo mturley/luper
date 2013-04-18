9a405efe496f3f290cd7d08b3d4efe93138928b7
Eric <ecsmith@student.umass.edu>
2013-04-18 18:54:44 -0400

    updated luperprojectactivity, starting to add DB sync

--- luper-android/src/com/teamluper/luper/LuperProjectEditorActivity.java
+++ luper-android/src/com/teamluper/luper/LuperProjectEditorActivity.java
@@ -22,61 +22,67 @@ import java.util.ArrayList;
 @EActivity
 public class LuperProjectEditorActivity extends SherlockActivity {
   SQLiteDataSource dataSource;
-	Sequence sequence = null;
+  Sequence sequence = null;
 
-	// TODO these will be moved to within Sequence, and accessed with
-	// sequence.getClips() and sequence.getTracks(), etc.
-	// ArrayList<Clip> clips = new ArrayList<Clip>();
-	// ArrayList<Track> tracks = new ArrayList<Track>();
+  // TODO these will be moved to within Sequence, and accessed with
+  // sequence.getClips() and sequence.getTracks(), etc.
+  // ArrayList<Clip> clips = new ArrayList<Clip>();
+  // ArrayList<Track> tracks = new ArrayList<Track>();
 
-	@Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+  @Override
+  public void onCreate(Bundle icicle) {
+    super.onCreate(icicle);
+    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-        long ID = getIntent().getLongExtra("selectedProjectId", -1);
-        if(ID == -1) {
-          DialogFactory.alert(this,"ERROR","No project ID found!  Aborting.",
-            new Lambda.VoidCallback() {
-              public void go() {
-                finish();
-              }
-            });
-          return;
-        }
-
-        dataSource = new SQLiteDataSource(this);
-        dataSource.open();
-
-        sequence = dataSource.getSequenceById(ID);
-        // TODO use the data within the sequence object to render the clips.
-        DialogFactory.alert(this, "Successfully Loaded Project ID #"+ID+
-          "!  The title is '"+sequence.getTitle()+"'");
-
-        // TODO: we only want to load stuff if we don't already have it loaded...
-        // onCreate gets called a bunch of times, so be careful only to load stuff once per open project
-        loadDataInBackground();
-        loadAudioInBackground();
-
-        final ActionBar bar = getSupportActionBar();
-        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS); // Gives us Tabs!
-
-        LinearLayout base = new LinearLayout(this);
-        base.setId(1337);
-        base.setBackgroundColor(Color.parseColor("#e2dfd8"));
-
-        base.setOrientation(LinearLayout.VERTICAL);
-
-        // RENDERING ROUTINE STARTS HERE
-        if(sequence.isReady()) {
-          // draw stuff in it
-          for(Track track : sequence.tracks) {
-            for(Clip clip : track.clips) {
-              // render the clip
-              // TODO test this, we have no real dummy data yet so I didn't test these loops
+    long ID = getIntent().getLongExtra("selectedProjectId", -1);
+    if(ID == -1) {
+      DialogFactory.alert(this,"ERROR","No project ID found!  Aborting.",
+          new Lambda.VoidCallback() {
+            public void go() {
+              finish();
             }
-          }
+          });
+      return;
+    }
+
+    dataSource = new SQLiteDataSource(this);
+    dataSource.open();
+
+    sequence = dataSource.getSequenceById(ID);
+    // TODO use the data within the sequence object to render the clips.
+    DialogFactory.alert(this, "Successfully Loaded Project ID #"+ID+
+        "!  The title is '"+sequence.getTitle()+"'");
+
+    // TODO: we only want to load stuff if we don't already have it loaded...
+    // onCreate gets called a bunch of times, so be careful only to load stuff once per open project
+    loadDataInBackground();
+    loadAudioInBackground();
+
+    final ActionBar bar = getSupportActionBar();
+    bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS); // Gives us Tabs!
+
+    LinearLayout base = new LinearLayout(this);
+    base.setId(1337);
+    base.setBackgroundColor(Color.parseColor("#e2dfd8"));
+
+    base.setOrientation(LinearLayout.VERTICAL);
+
+    // RENDERING ROUTINE STARTS HERE
+    if(sequence.isReady()) {
+      // draw stuff in it
+      for(Track track : sequence.tracks) {
+        RelativeLayout track1 = new RelativeLayout(this);
+        base.addView(track1,
+            new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+        for(Clip clip : track.clips) {
+          track1.addView(track1);
+          // render the clip
+          // TODO test this, we have no real dummy data yet so I didn't test these loops
         }
+      }
+    }
 
 
 
@@ -85,11 +91,11 @@ public class LuperProjectEditorActivity extends SherlockActivity {
 
 
         /* TESTS FUNCTIONALLITY OF THE TRACK VIEW RENDERING */
-          Clip clip1 = new Clip(); clip1.begin = 0; clip1.end = 500; clip1.duration = 500;
-          Clip clip2 = new Clip(); clip2.begin = 250; clip2.end = 1000; clip2.duration = 650;
-          Clip clip3 = new Clip(); clip3.begin = 100; clip3.end = 600; clip3.duration = 500;
+    Clip clip1 = new Clip(); clip1.begin = 0; clip1.end = 500; clip1.duration = 500;
+    Clip clip2 = new Clip(); clip2.begin = 250; clip2.end = 1000; clip2.duration = 650;
+    Clip clip3 = new Clip(); clip3.begin = 100; clip3.end = 600; clip3.duration = 500;
 
-        TrackView track1 = new TrackView(this);
+//        TrackView track1 = new TrackView(this);
 
 //        ImageButton addClipButton = new ImageButton(this);
 //        addClipButton.setImageResource(R.drawable.add);
@@ -99,10 +105,6 @@ public class LuperProjectEditorActivity extends SherlockActivity {
 //        RelativeLayout track1 = new RelativeLayout(this);
 //        RelativeLayout track2 = new RelativeLayout(this);
 //        RelativeLayout track3 = new RelativeLayout(this);
-//
-//        ColorChipButton chip1 = new ColorChipButton(this, clip1);
-//        ColorChipButton chip2 = new ColorChipButton(this, clip2);
-//        ColorChipButton chip3 = new ColorChipButton(this, clip3);
 //
 //        track1.addView(chip1);
 //        track2.addView(chip2);
@@ -116,13 +118,13 @@ public class LuperProjectEditorActivity extends SherlockActivity {
 //        chip2.setBackgroundColor(Color.RED);
 //        chip3.setBackgroundColor(Color.WHITE);
 
-        TextView timelinetxt = new TextView(this);
-        timelinetxt.setText("_____|__________________|___________________|___________________|___________________|_________\n");
+//        TextView timelinetxt = new TextView(this);
+//        timelinetxt.setText("_____|__________________|___________________|___________________|___________________|_________\n");
 
-        base.addView(track1,
-                new RelativeLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT));
+//        base.addView(track1,
+//                new RelativeLayout.LayoutParams(
+//                        ViewGroup.LayoutParams.WRAP_CONTENT,
+//                        ViewGroup.LayoutParams.WRAP_CONTENT));
 //        base.addView(track2,
 //                new RelativeLayout.LayoutParams(
 //                        ViewGroup.LayoutParams.WRAP_CONTENT,
@@ -132,9 +134,9 @@ public class LuperProjectEditorActivity extends SherlockActivity {
 //                        ViewGroup.LayoutParams.WRAP_CONTENT,
 //                        ViewGroup.LayoutParams.WRAP_CONTENT));
 
-        setContentView(base);
+    setContentView(base);
 
-    }
+  }
 
   @Override
   protected void onStop() {
@@ -148,16 +150,16 @@ public class LuperProjectEditorActivity extends SherlockActivity {
     super.onResume();
   }
 
-	// #Creates the Actionbar
-	@Override
+  // #Creates the Actionbar
+  @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     MenuInflater inf = getSupportMenuInflater();
     inf.inflate(R.menu.editor_bar, menu);
     return super.onCreateOptionsMenu(menu);
   }
-	@Override
+  @Override
   public boolean onOptionsItemSelected(MenuItem item) {
-	  boolean incomplete = false;
+    boolean incomplete = false;
     if(item.getItemId() == R.id.editor_play) {
       // TODO
       incomplete = true;
