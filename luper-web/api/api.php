a9b997dbe5a0f444e4b3730aa1d5e3f5dea763fb
Mike Turley <mike@miketurley.com>
2013-04-19 16:21:30 -0400

    Wrote all Register/Login related server-side code and the corresponding LuperRestClient methods.
    Also added Google's GSON as a dependency, for encoding and decoding JSON requests and responses.

--- luper-web/api/api.php
+++ luper-web/api/api.php
@@ -25,14 +25,18 @@ $api->get('/test', function() use ($api) {
   }
 });
 
-// /auth/register/  registers a new user account
-$api->post('/auth/register/:email/:plainTextPassword/:username',
-  function($email, $plainTextPassword, $username) use ($api) {
+// /auth/register  registers a new user account
+$api->post('/auth/register',
+  function() use ($api) {
   try {
-    $input = $api->request()->getBody();
-    // TODO fetch actual values from the body
+    // fetch and decode the request object
+    $request = json_decode($api->request()->getBody());
+    $email = $request->email;
+    $password = $request->password;
+    $username = $request->username;
+    // put some data into the database
     $db = getDB();
-    $hash = newRandomSaltedHash($email, $plainTextPassword);
+    $hash = newRandomSaltedHash($email, $password);
     $stmt = $db->prepare(
       "INSERT INTO Users (username, email, passwordHash, isActiveUser, preferences)".
       "           VALUES (:username, :email, :passwordHash, 1, '{}');");
@@ -40,55 +44,95 @@ $api->post('/auth/register/:email/:plainTextPassword/:username',
     $stmt->bindParam("email",$email);
     $stmt->bindParam("passwordHash",$hash);
     $stmt->execute();
+    // build, encode and send the response object
     $response = new stdclass();
     $response->success = true;
     $response->insertId = $db->lastInsertId();
     echo json_encode($response);
   } catch(PDOException $e) {
+    // database errors will be delivered as an HTTP 500 Internal Server Error
     $api->halt(500,$e->getMessage());
   }
 });
 
-// /auth/passwd/  resets and/or changes a password
-$api->post('/auth/passwd/:email/:newPlainTextPassword', function() use ($api) {
+// /auth/challenge  retrieves the challenge salt value for a given user
+$api->post('/auth/challenge', function($email) use ($api) {
   try {
-    // TODO update query via PDO, respond with success JSON
-  } catch(PDOException $e) {
-    $api->halt(500,$e->getMessage());
-  }
-});
-
-// /auth/login/  validates a login attempt with the half-baked approach (see auth.php)
-// better security with or without HTTPS, but slower than below.
-$api->get('/auth/login/:email/:halfBakedPasswordHash', function() use ($api) {
-  try {
-    // TODO
-  } catch(PDOException $e) {
-    $api->halt(500,$e->getMessage());
-  }
-});
-
-// /auth/login_insecure/  validates a login attempt with plain text submission.
-// acceptable security if used exclusively over HTTPS.  Faster than above.
-$api->get('/auth/login_insecure/:email/:plainTextPassword', function() use ($api) {
-  try {
-    // TODO
-  } catch(PDOException $e) {
-    $api->halt(500,$e->getMessage());
-  }
-});
-
-// /auth/salt/  retrieves the salt value for a given user
-// for use in performing a client-side partial hash ("half-baked hash")
-$api->get('/auth/salt/:email', function($email) use ($api) {
-  try {
-    $hash = getKnownHashForUser($email);
+    // TODO refactor this so every challenge salt is different... we'd need to store a different password hash too
+    $db = getDB();
+    $hash = getKnownHashForUser($db, $email);
     $salt = substr($hash, 0, 64);
     $response = new stdclass();
     $response->email = $email;
     $response->salt = $salt;
     echo json_encode($response);
   } catch(PDOException $e) {
+    $api->halt(500,$e->getMessage());
+  }
+});
+
+// /auth/login  validates a login attempt against the database.
+$api->post('/auth/login', function() use ($api) {
+  try {
+    // fetch and decode the request object
+    $request = json_decode($api->request()->getBody());
+    $email = $request->email;
+    $password = $request->password; // may be a hash or plain text depending on...
+    $securityType = $request->securityType; // this.
+    // validate the password against the database
+    $db = getDB();
+    $isLoginValid = false;
+    if($securityType == "plaintext") {
+      $isLoginValid = validatePlainTextLoginAttempt($db, $email, $password);
+    } else if($securityType == "halfbaked") {
+      $isLoginValid = validateHalfBakedLoginAttempt($db, $email, $password);
+    }
+    // build, encode and send the response object
+    $response = new stdclass();
+    $response->isLoginValid = $isLoginValid;
+    echo json_encode($response);
+  } catch(PDOException $e) {
+    // database errors will be delivered as an HTTP 500 Internal Server Error
+    $api->halt(500,$e->getMessage());
+  }
+});
+
+// /auth/passwd  resets and/or changes a password
+$api->post('/auth/passwd', function() use ($api) {
+  try {
+    // fetch and decode the request object
+    $request = json_decode($api->request()->getBody());
+    $email = $request->email;
+    $oldPassword = $request->oldPassword; // depends on securityType
+    $newPassword = $request->newPassword; // plain text
+    $securityType = $request->securityType;
+    // validate the old password against the database
+    $db = getDB();
+    $oldPasswordIsValid = false;
+    if($securityType == "plaintext") {
+      $oldPasswordIsValid = validatePlainTextLoginAttempt($db, $email, $oldPassword);
+    } else if($securityType == "halfbaked") {
+      $oldPasswordIsValid = validateHalfBakedLoginAttempt($db, $email, $oldPassword);
+    }
+    if(!$oldPasswordIsValid) {
+      $api->halt(403,"FORBIDDEN: Incorrect Old Password.");
+    } else {
+      $newHash = newRandomSaltedHash($email, $newPassword);
+      $stmt = $db->prepare("UPDATE Users SET passwordHash = :hash WHERE email = :email");
+      $stmt->bindParam("hash",$newHash);
+      $stmt->bindParam("email",$email);
+      $stmt->execute();
+      // build, encode and send the response object
+      $response = new stdclass();
+      if($stmt->rowCount() == 1) {
+        $response->success = true;
+      } else {
+        $response->success = false;
+      }
+      echo json_encode($response);
+    }
+  } catch(PDOException $e) {
+    // database errors will be delivered as an HTTP 500 Internal Server Error
     $api->halt(500,$e->getMessage());
   }
 });
