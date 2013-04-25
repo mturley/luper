f445bfcaf83470a97c554e168a8643fdcbca6229
Mike Turley <mike@miketurley.com>
2013-04-25 16:28:57 -0400

    adding some comments

--- luper-android/src/com/teamluper/luper/TabRegisterFragment.java
+++ luper-android/src/com/teamluper/luper/TabRegisterFragment.java
@@ -13,7 +13,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import com.googlecode.androidannotations.annotations.Background;
 import com.googlecode.androidannotations.annotations.EFragment;
@@ -170,7 +169,7 @@ public class TabRegisterFragment extends Fragment {
           a.prefillLoginForm(e);
         }
       });
-  } 
+  }
 
   @UiThread
   public void registrationFailure(String errorMessage) {
