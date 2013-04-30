3cde841b758ee781d539e0b376e97ecd64e29deb
Mike Turley <mike@miketurley.com>
2013-04-29 21:46:53 -0400

    adding getAudioFilesByUserId

--- luper-android/src/com/teamluper/luper/TrackView.java
+++ luper-android/src/com/teamluper/luper/TrackView.java
@@ -155,7 +155,6 @@ public class TrackView extends RelativeLayout {
 
         	System.out.println("Here " + this.associated.getClips().get(i).begin);
         	chip = new ColorChipButton(this.getContext(), this.associated.getClips().get(i));
-          chip.setRandColor();
         	System.out.println("Chips x pos " + chip.associated.begin);
             this.addView(chip);
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
