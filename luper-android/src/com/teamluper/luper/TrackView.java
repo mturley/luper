a4874ffcfee46cd710a4967f2bc2a02da0ae7126
xianc <xianc@student.umass.edu>
2013-04-30 21:27:59 -0400

    luper help shtuff

--- luper-android/src/com/teamluper/luper/TrackView.java
+++ luper-android/src/com/teamluper/luper/TrackView.java
@@ -138,14 +138,14 @@ public class TrackView extends RelativeLayout {
 		ImageButton addClipButton = new ImageButton(this.getContext());
 		addClipButton.setImageResource(R.drawable.add);
 		addClipButton.setOnClickListener(addClipClicker);
-        addClipButton.setBackgroundColor(Color.parseColor("#e2dfd8"));
+        addClipButton.setBackgroundColor(Color.parseColor("#f5f5f5"));
 		trackControl.addView(addClipButton);
 
 //		create the playButton then set its image to play and add it to the trackControl
 		ImageButton playButton = new ImageButton(this.getContext());
 		playButton.setImageResource(R.drawable.play);
 		playButton.setOnClickListener(playClicker);
-        playButton.setBackgroundColor(Color.parseColor("#e2dfd8"));
+        playButton.setBackgroundColor(Color.parseColor("#f5f5f5"));
 		trackControl.addView(playButton);
 
         //trackControl.setBackgroundColor(Color.parseColor("#000000"));
