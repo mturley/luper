3ab78d1b952121ed6e9bdcce1bb7213a61f80c89
Mike Turley <mike@miketurley.com>
2013-05-02 19:55:02 -0400

    another mobile site fix.  gotta push it to test it...

--- luper-web/index-mobile.php
+++ luper-web/index-mobile.php
@@ -8,13 +8,13 @@
     <div id="container" align="center">
       <div id="header">
         <img src="luper.png" style="width: 90%"/><br />
-        1.0 Release
+        <strong>1.0 Release</strong><br /><br />
       </div>
       <div id="content">
         <h2 class="tap"><a href="/download/Luper-1.0.apk">Tap to Download:<br /><img src="ic_launcher.png" /><br />Luper-1.0.apk</a></h2>
-        <h3 class="darkred">NOTE: Your device must be set to allow installation of applications from "Unknown Sources" (found in Settings > Security)</h3>
-        <h4><a href="index.php?full=1">Tap Here for Full Website</a></h4>
+        <h4 class="darkred">NOTE: Your device must be set to allow installation of applications from "Unknown Sources" (found in Settings > Security)</h4>
       </div>
+      <h4><a href="index.php?full=1">Tap Here for Full Website</a></h4>
     </div>
   </body>
 </html>
