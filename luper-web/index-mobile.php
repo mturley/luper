df816feccad5ca234cf1d98808e8a0ed81d55a20
Mike Turley <mike@miketurley.com>
2013-05-02 19:36:06 -0400

    style improvements to mobile site

--- luper-web/index-mobile.php
+++ luper-web/index-mobile.php
@@ -1,11 +1,20 @@
 <html>
   <head>
     <title>Luper mobile site</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel='stylesheet' href='style.css' />
   </head>
   <body>
-    <h1>Luper 1.0 Release (5/1/2013)</h1>
-    <h2><a href="/download/Luper-1.0.apk">Click Here to Download Luper-1.0.apk</a></h2>
-    <h3>For more details, visit this page on a desktop browser.</h3>
-    <h4>(proper mobile site style coming soon)</h4>
+    <div id="container" align="center">
+      <div id="header">
+        <h1>Luper 1.0 Release</h1>
+      </div>
+      <div id="content">
+        <h2 class="tap"><a href="/download/Luper-1.0.apk">Tap to Download:<br /><img src="ic_launcher.png" /><br />Luper-1.0.apk</a></h2>
+        <h3 class="darkred">NOTE: Your device must be set to allow installation of applications from "Unknown Sources" (found in Settings > Security)</h3>
+        <h3>For more details, visit this page on a desktop browser.</h3>
+        <h4><a href="index.php?full=1">Tap Here for Full Website</a></h4>
+      </div>
+    </div>
   </body>
 </html>
