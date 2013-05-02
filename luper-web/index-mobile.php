66ab50d23caec5e0c627a693832cd89d05f74017
Mike Turley <mike@miketurley.com>
2013-05-02 19:44:54 -0400

    removing a redundant line from the mobile website

--- luper-web/index-mobile.php
+++ luper-web/index-mobile.php
@@ -12,7 +12,6 @@
       <div id="content">
         <h2 class="tap"><a href="/download/Luper-1.0.apk">Tap to Download:<br /><img src="ic_launcher.png" /><br />Luper-1.0.apk</a></h2>
         <h3 class="darkred">NOTE: Your device must be set to allow installation of applications from "Unknown Sources" (found in Settings > Security)</h3>
-        <h3>For more details, visit this page on a desktop browser.</h3>
         <h4><a href="index.php?full=1">Tap Here for Full Website</a></h4>
       </div>
     </div>
