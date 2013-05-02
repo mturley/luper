fc72e4fd90626524d709bc804c30116cdad11f61
Mike Turley <mike@miketurley.com>
2013-05-02 19:09:08 -0400

    style updates to website

--- luper-web/header.php
+++ luper-web/header.php
@@ -1,20 +1,22 @@
 <?php
 
-function button($url, $label) {
+function button($url, $label, $icon = null) {
   echo '<button type="button" ';
   if($_SERVER['PHP_SELF'] == $url) echo 'class="active"';
-  echo '><a href="'.$url.'">'.$label.'</a></button>';
+  echo '>';
+  if($icon != null) echo '<img src="'.$icon.'" />';
+  echo '<a href="'.$url.'">'.$label.'</a></button>';
 }
 
 ?>
 
 <div id="header">
   <?php
-    button("/index.php","Home (1.0 Release)");
-    button("/beta.php","Archived Beta");
-    button("/alpha.php","Archived Alpha");
-    button("/download/Luper-1.0.apk","Download APK");
-    button("/feedback.php","Feedback");
+    button("/index.php","Home (1.0 Release)","png/9_av_play.png");
+    button("/beta.php","Archived Beta","png/9_av_rewind.png");
+    button("/alpha.php","Archived Alpha","png/9_av_previous.png");
+    button("/download/Luper-1.0.apk","Download APK","png/9_av_download.png");
+    button("/feedback.php","Feedback","png/5_content_email.png");
   ?>
 </div>
 
